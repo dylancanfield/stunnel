a1f2cb5c1117698bd666ce00dd8fb10abb17fc1d
Dylan Canfield <Dylan.Canfield@colorado.edu>
2016-03-14 17:36:30 -0600

    Fixed template notifies for stunnel4 service

--- recipes/default.rb
+++ recipes/default.rb
@@ -49,7 +49,7 @@ template "/etc/stunnel/stunnel.conf" do
   source "stunnel.conf.erb"
   mode 0644
   action :nothing
-  notifies :restart, 'service[stunnel]', :delayed
+  notifies :restart, 'service[stunnel4]', :delayed
 end
 
 template "/etc/default/stunnel4" do
