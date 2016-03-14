453f38e48236b869152cead96a6d1f1897ab2217
Dylan Canfield <Dylan.Canfield@colorado.edu>
2016-03-14 11:33:25 -0600

    Cleaning up environment stuff

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             "stunnel"
-maintainer       "Heavy Water"
-maintainer_email "support@hw-ops.com"
+maintainer       "dylancanfield"
+maintainer_email "dylan.canfield@colorado.edu"
 license          "Apache 2.0"
 description      "Installs/Configures stunnel"
 version          "2.2.0"
